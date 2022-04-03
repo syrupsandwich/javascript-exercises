6d3e4d9f599f131b0294311fc9f17038c084e3aa
syrupsandwich <dontsendsamspam@gmail.com>
2022-04-02 17:25:13 -0700

    Save current progress up to exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(numbers, target) {
+  let array = [];
 
+    for(let i = 0; i < numbers.length ; i++) {
+        if (target === numbers[i]){
+          continue;
+        } else {
+        array.push(numbers[i]);
+        }
+      }
+  return array;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
