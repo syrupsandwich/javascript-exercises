9c68e2d8b14e8cbab1d612ae2bf6d225c57c9bc9
syrupsandwich <dontsendsamspam@gmail.com>
2022-04-02 17:27:41 -0700

    Save previous work up to exercise 04

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string,number) {
+let fatAlbert = '';
+if(number < 0){
+fatAlbert = 'ERROR';
+} else {
+for(let i = 0; i < number ; i++){
+      fatAlbert += string;
+  }
+}
+return fatAlbert;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let stringInReverse = '';
+  let newString = string;
+  for(let i = string.length; i > -1 ; i--) {
+    stringInReverse += newString.slice(-1);
+    newString = string.slice(0, i - 1);
+  }
+  return stringInReverse;
 };
 
 // Do not edit below this line
