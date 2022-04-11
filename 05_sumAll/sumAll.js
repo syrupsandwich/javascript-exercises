d43c6514ab716483081a6c737d9171de21a590e9
syrupsandwich <dontsendsamspam@gmail.com>
2022-04-11 10:44:35 -0700

    save work for exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+  if(num1 < num2) {
+    sum = num1;
+    end = num2;
+  } else {
+    sum = num2;
+    end = num1;
+  }
+  for(let i = (sum + 1) ; i <= end ; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
