5b697b67836eb28f081cd3daff56618b085d0b56
syrupsandwich <dontsendsamspam@gmail.com>
2022-04-11 13:02:19 -0700

    add solutions for exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
 const sumAll = function(num1, num2) {
-  if(num1 < num2) {
+  if(num1 < 0 || num2 < 0) {
+    return "ERROR";
+  } else if(num1 + 1 !== +num1 + 1 || num2 + 1 !== +num2 + 1) {
+    return "ERROR";
+  } else if(num1 < num2) {
     sum = num1;
     end = num2;
   } else {
