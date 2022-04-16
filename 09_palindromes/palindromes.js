4a34c3cd3610639f33df8cd3dbc903ea46d271d4
syrupsandwich <dontsendsamspam@gmail.com>
2022-04-16 15:48:11 -0700

    add solutions for exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
-
+const palindromes = function (phrase) {
+  let string = phrase.toLowerCase();
+  let filteredString = '';
+  let stringInReverse = '';
+  for(let i = 0; i <= string.length ; i++) {
+    if(string.substr(i, 1) == ' '
+    || string.substr(i, 1) == '!'
+    || string.substr(i, 1) == ','
+    || string.substr(i, 1) == '.') {
+      continue;
+    } else {
+    filteredString += string.substr(i, 1);
+    }
+  }
+  for(let i = 1; i <= filteredString.length ; i++) {
+    stringInReverse += filteredString.substr(i * -1, 1);
+    }
+  return (filteredString == stringInReverse ? true : false);
 };
 
 // Do not edit below this line
