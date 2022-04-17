de78394084563b2519dfd993f11a73bae50dee7e
syrupsandwich <dontsendsamspam@gmail.com>
2022-04-17 16:15:11 -0700

    add solutions for exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  if(num < 0) {
+    return 'OOPS';
+  }
+  let firstNum = 0;
+  let secondNum = 1;
+  let result = 0;
+  for(let i = 1; i < num ; i++) {
+    result = firstNum + secondNum;
+    firstNum = secondNum;
+    secondNum = result;
+  }
+return secondNum;
 };
 
 // Do not edit below this line
